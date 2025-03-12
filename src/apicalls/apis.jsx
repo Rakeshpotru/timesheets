d6303a7b5b8cd13580e3699f225fde2ae899bac7
Sowjanya <sowjanyan@newmeksolutions.com>
2025-03-12 16:45:53 +0530

    commit

--- src/apicalls/apis.jsx
+++
@@ -1,6 +0,0 @@
-export const API_URLS = {
-    LOGIN_API_URL : 'https://5hdtduhto9.execute-api.us-east-1.amazonaws.com/login',
-    REGISTER_API_URL:'https://3fj3uno4tb.execute-api.us-east-1.amazonaws.com/Registration',
-    ADDEMPLOYEE_API_URL:"https://oc6aiyt844.execute-api.us-east-1.amazonaws.com/AddEmployee",
-    ADDTIMESHEETS_API_URL:"https://vpahj6fmbk.execute-api.us-east-1.amazonaws.com/Timesheets",
-}
